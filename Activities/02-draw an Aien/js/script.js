1a32f854a4c4e3c208e0b1fb8a619280f47f90b7
PeekyPaprika <54860210+PeekyPaprika@users.noreply.github.com>
2020-09-15 13:29:03 -0400

    Made body and face of alien guy

--- Activities/02-draw an Aien/js/script.js
+++ Activities/02-draw an Aien/js/script.js
@@ -9,6 +9,20 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+createCanvas(640,480);
+background(255,192,203);
+noStroke();
+
+// Body
+fill(120);
+ellipse(320, 480, 500, 300);
+
+//Head thing
+fill(130);
+ellipse(320, 210, 300, 400);
+
+//Billie EYES
+
 
 }
 
